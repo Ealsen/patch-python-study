d83104f4e0097c61f5d424794256401ba7ac1b3c
Ealsen_Huangfu <111371035+Ealsen@users.noreply.github.com>
2025-01-30 14:47:45 +0800

    new test

--- is_test_code/test_code_eg1.py
+++ is_test_code/test_code_eg1.py
@@ -1,5 +1,15 @@
-import os
+# This is a test code for variable class test.
 
+def test_func_variable_class_test():
+    
+    """
+    This is a test function for variable class test.
+    """
+    test_func_name: str = "variable class test."
+    arrary1 = [{"name": "John", "age": 25},{"work":[{"company": "Google", "position": "Software Engineer"}]}]
+    print(arrary1)
+    # print(arrary1.length) # This will give an error as length is not a property of an array.
 
-current_dir = os.getcwd()
-print(current_dir)
+if __name__ == "__main__":
+    # test_func_variable_class_test()
+    pass
